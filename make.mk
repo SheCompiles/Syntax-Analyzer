f32225955802380df4bf6458d39c4a692ae21165
Patriz Elaine Daroy <33110041+patrizelaine@users.noreply.github.com>
2020-11-23 22:08:29 -0800

    Updating to proper lex file

--- make.mk
+++ make.mk
@@ -1,8 +1,8 @@
 syntaxAnalyzer: lex.yy.c y.tab.c
 	gcc lex.yy.c y.tab.c -ly -ll
 
-lex.yy.c: y.tab.c lexical2.l
-	lex lexical2.l
+lex.yy.c: y.tab.c lexical.l
+	lex lexical.l
 
 y.tab.c: parser.y
 	yacc -dyv parser.y
